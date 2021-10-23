0c2e8f7ec5b14b23fecca842444585ec3898867b
Matt <matt@stallings.us>
2021-10-23 11:31:29 -0500

    Initial commit

---
+++ src/main/java/us/stallings/ssch6/Ssch6Application.java
@@ -0,0 +1,13 @@
+package us.stallings.ssch6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ssch6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ssch6Application.class, args);
+    }
+
+}
---
+++ src/test/java/us/stallings/ssch6/Ssch6ApplicationTests.java
@@ -0,0 +1,13 @@
+package us.stallings.ssch6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ssch6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
